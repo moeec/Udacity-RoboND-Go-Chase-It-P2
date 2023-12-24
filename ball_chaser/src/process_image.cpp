d9ab8d7f5a1fa96af471271b3a546873c5d39aca
Mwesi <mwesi@yahoo.com>
2023-12-24 07:40:37 -0500

    Update process_image.cpp

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -27,15 +27,31 @@ void drive_robot(float lin_x, float ang_z)
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
+    const int white_pixel = 255;
 
-    int white_pixel = 255;
+    // Iterate through every pixel in the image
+    for (int i = 0; i < img.height * img.step; i += 3)
+    {
+        // Check for white pixel
+        if (img.data[i] == white_pixel && img.data[i + 1] == white_pixel && img.data[i + 2] == white_pixel)
+        {
+            int column_index = i % img.step;
 
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
-    // Request a stop when there's no white ball seen by the camera
+            // Determine the direction based on the column index
+            if (column_index < img.step / 3)
+                drive_robot(0.5, 1); // Turn left
+            else if (column_index < 2 * img.step / 3)
+                drive_robot(0.5, 0); // Move forward
+            else
+                drive_robot(0.5, -1); // Turn right
+
+            return; // Exit after finding the ball
+        }
+    }
+
+    // If no white pixel found, stop the robot
+    drive_robot(0, 0);
 }
-
 int main(int argc, char** argv)
 {
     // Initialize the process_image node and create a handle to it
